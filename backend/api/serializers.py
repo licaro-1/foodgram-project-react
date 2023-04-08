2a737a3b897038b8d843f0202a84a411c91bc8cb
licaro-1 <dlishuhai@yandex.ru>
2023-04-08 18:02:15 +0300

    set password fix

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework import serializers, status
+from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 from rest_framework.exceptions import ValidationError
 
